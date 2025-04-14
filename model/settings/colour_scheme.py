01648541a01591e59ae4bf28565676a15bebdcf4
Patrick Schwizer <schwizer.p@proton.me>
2025-04-14 13:32:08 +0200

    * add root node highlighting

--- model/settings/colour_scheme.py
+++ model/settings/colour_scheme.py
@@ -2,15 +2,17 @@ from enum import Enum
 
 
 class ColorRole(Enum):
-    PRECEDENT = "precedent"
     DEPENDENT = "dependent"
     HEATMAP_0 = "heatmap_0"
     HEATMAP_1 = "heatmap_1"
+    PRECEDENT = "precedent"
+    ROOT_NODE = "root_node"
 
 
 ColourScheme: dict[ColorRole, str] = {
-    ColorRole.PRECEDENT: "#ff9999",  # light red
     ColorRole.DEPENDENT: "#99ff99",  # light green
     ColorRole.HEATMAP_0: "#ffffcc",  # soft yellow
     ColorRole.HEATMAP_1: "#ff4444",  # bold red
+    ColorRole.PRECEDENT: "#ff9999",  # light red
+    ColorRole.ROOT_NODE: "#cce5ff"
 }
