ee155700c0f43a7debc8b44375f8860450729499
Patrick Schwizer <schwizer.p@proton.me>
2025-05-18 20:52:04 +0200

    * add diagrams * small refactoring of dependency_graph.py * improved README.md

--- src/model/domain_model/spreadsheet/dependency_graph.py
+++ src/model/domain_model/spreadsheet/dependency_graph.py
@@ -9,16 +9,16 @@ from model.domain_model.spreadsheet.range_reference import RangeReference, Range
 
 @dataclass(frozen=True)
 class DependencyGraph:
-    graph: nx.DiGraph
+    _graph: nx.DiGraph
 
     def get_cell_range(self, range_ref: RangeReference) -> Optional[CellRange]:
-        if range_ref not in self.graph:
+        if range_ref not in self._graph:
             return None
-        return self.graph.nodes.get(range_ref).get('cell_range')
+        return self._graph.nodes.get(range_ref).get('cell_range')
 
     def get_cell_ranges(self) -> set[CellRange]:
         cell_ranges: set[CellRange] = set()
-        for cell_reference in self.graph.nodes:
+        for cell_reference in self._graph.nodes:
             cell = self.get_cell_range(cell_reference)
             if cell:
                 cell_ranges.add(cell)
@@ -30,36 +30,38 @@ class DependencyGraph:
     def has_dependent(self, range_ref: RangeReference) -> bool:
         return len(self.get_dependents(range_ref)) > 0
 
-    def resolve_dependents_to_cell_level(self,
-                                         range_ref: RangeReference,
-                                         resolved_dependents: set[RangeReference] = None) -> set[RangeReference]:
-        if resolved_dependents is None:
-            resolved_dependents = set()
-        for dependent in self.get_dependents(range_ref):
-            if dependent.reference_type == RangeReferenceType.CELL:
-                resolved_dependents.add(dependent)
-            elif dependent.reference_type != RangeReferenceType.EXTERNAL:
-                resolved_dependents.update(self.resolve_dependents_to_cell_level(dependent, resolved_dependents))
+    def resolve_dependents_to_cell_level(self, range_ref: RangeReference) -> set[RangeReference]:
+        resolved_dependents = set()
+
+        def _resolve(current_ref: RangeReference):
+            for dependent in self.get_dependents(current_ref):
+                if dependent.reference_type == RangeReferenceType.CELL:
+                    resolved_dependents.add(dependent)
+                elif dependent.reference_type != RangeReferenceType.EXTERNAL:
+                    _resolve(dependent)
+
+        _resolve(range_ref)
         return resolved_dependents
 
-    def resolve_precedents_to_cell_level(self,
-                                         range_ref: RangeReference,
-                                         resolved_precedents: set[RangeReference] = None) -> set[RangeReference]:
-        if resolved_precedents is None:
-            resolved_precedents = set()
-        for precedent in self.get_precedents(range_ref):
-            if precedent.reference_type == RangeReferenceType.CELL:
-                resolved_precedents.add(precedent)
-            elif precedent.reference_type != RangeReferenceType.EXTERNAL:
-                resolved_precedents.update(self.resolve_precedents_to_cell_level(precedent, resolved_precedents))
-        return resolved_precedents
+    def resolve_precedents_to_cell_level(self, range_ref: RangeReference) -> set[RangeReference]:
+        resolved_dependents = set()
+
+        def _resolve(current_ref: RangeReference):
+            for dependent in self.get_precedents(current_ref):
+                if dependent.reference_type == RangeReferenceType.CELL:
+                    resolved_dependents.add(dependent)
+                elif dependent.reference_type != RangeReferenceType.EXTERNAL:
+                    _resolve(dependent)
+
+        _resolve(range_ref)
+        return resolved_dependents
 
     def get_precedents(self, cell: RangeReference) -> set[RangeReference]:
-        if cell in self.graph:
-            return set(self.graph.predecessors(cell))
+        if cell in self._graph:
+            return set(self._graph.predecessors(cell))
         return set()
 
     def get_dependents(self, cell: RangeReference) -> set[RangeReference]:
-        if cell in self.graph:
-            return set(self.graph.successors(cell))
+        if cell in self._graph:
+            return set(self._graph.successors(cell))
         return set()
